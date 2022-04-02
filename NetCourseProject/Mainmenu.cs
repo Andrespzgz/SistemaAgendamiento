e93965279f8195d14cda63c7eee01ea85f5a8220
CamiloAndrespzgz <81250560+CamiloAndresPerez0111@users.noreply.github.com>
2022-04-01 22:07:55 -0500

    Start

---
+++ NetCourseProject/Mainmenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace NetCourseProject
+{
+    public class Mainmenu
+    {
+        //private string _dato;
+        //public Mainmenu(string dato) => (_dato) = (dato);
+        public List<string> lista;
+        public Mainmenu datos()
+        {
+            List<string> list = new List<string>();
+            list.Add("1-Añadir cita");
+            list.Add("2-Añadir contacto");
+            list.Add("3-Consultar cita");
+            list.Add("4-Buscar contacto");
+            list.Add("0-Salir");
+            
+            return  null;
+
+        }
+        
+        
+
+        
+    }
+}
---
+++ NetCourseProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using static System.Convert;
+
+namespace FinalProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Bienvenido a su sistema de agendamiento");
+            WriteLine("");
+            List<string> list = new List<string>();
+            list.Add("1-Añadir cita");
+            list.Add("2-Añadir contacto");
+            list.Add("3-Consultar cita");
+            list.Add("4-Buscar contacto");
+            list.Add("0-Salir");
+
+
+            foreach (string s in list)
+                WriteLine(s);
+
+            WriteLine("");
+            Write("Por favor ingrese la opcion a consular: ");
+            var numSeleccionado = ReadLine();
+
+            WriteLine(numSeleccionado);
+            ReadKey();
+        }
+    }
+}
